f06af8d984d45f25aed54772daa23eb7390f03ce
Leonardo Lopes <leonardolivelopes2@gmail.com>
2023-12-13 15:51:00 -0300

    seller entity class

--- src/App.java
+++ src/App.java
@@ -1,8 +1,14 @@
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Department department = new Department(1, "Books");
         System.out.println(department);
+
+        Seller seller = new Seller(1, "Leonardo", "leonardo@gmail.com", new Date(), 50000.00, department);
+        System.out.println(seller);
     }
 }
