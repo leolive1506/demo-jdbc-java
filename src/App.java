499a12079366eeee52ee006b99b99e9be1340ea5
Leonardo Lopes <leonardolivelopes2@gmail.com>
2023-12-13 16:54:33 -0300

    findById implementation

--- src/App.java
+++ src/App.java
@@ -1,18 +1,11 @@
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Department department = new Department(1, "Books");
-        System.out.println(department);
-
-        Seller seller = new Seller(1, "Leonardo", "leonardo@gmail.com", new Date(), 50000.00, department);
-        System.out.println(seller);
-        
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,16 +1,28 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
+  private Connection conn;
+
+  public SellerDaoJDBC(Connection conn) {
+    this.conn = conn;
+  }
 
   @Override
   public void deleteById(Integer id) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
@@ -21,20 +33,52 @@ public class SellerDaoJDBC implements SellerDao {
 
   @Override
   public Seller findById(Integer id) {
-    // TODO Auto-generated method stub
-    return null;
+    PreparedStatement st = null;
+    ResultSet rs = null;
+    try {
+      st = conn.prepareStatement(
+          "SELECT seller.*,department.Name as DepName FROM seller INNER JOIN department ON seller.DepartmentId = department.Id WHERE seller.Id = ?");
+      st.setInt(1, id);
+      // retorna em formato tabela, posição 0 não contém objeto
+      rs = st.executeQuery();
+
+      // check se veio algum objeto
+      if (rs.next()) {
+        Department department = new Department(
+            rs.getInt("DepartmentId"),
+            rs.getString("DepName"));
+
+        Seller seller = new Seller(
+          rs.getInt("Id"),
+          rs.getString("Name"),
+          rs.getString("Email"),
+          rs.getDate("BirthDate"),
+          rs.getDouble("BaseSalary"),
+          department
+        );
+
+        return seller;
+      }
+
+      return null;
+    } catch (SQLException e) {
+      throw new DbException(e.getMessage());
+    } finally {
+      DB.closeStatement(st);
+      DB.closeResultSet(rs);
+    }
   }
 
   @Override
   public void insert(Seller obj) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void update(Seller obj) {
     // TODO Auto-generated method stub
-    
+
   }
-  
+
 }
